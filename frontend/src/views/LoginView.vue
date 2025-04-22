49405a9ca5929fd3158c6d46a353be03f1ed395b
alexandremorin24 <9761@holbertonstudents.com>
2025-04-22 16:33:21 +0200

    UI setup with Vuetify + initial pages (Home, Login, Register)
    - Installed Vuetify 3
    - Integrated Material Design Icons (mdi) for icon support
    - Created HomeView.vue with Vuetify card + welcome message
    - Added router links and placeholders for Login and Register views
    
    Frontend now ready for form integration and API connections

---
+++ frontend/src/views/LoginView.vue
@@ -0,0 +1,37 @@
+<template>
+  <v-container>
+    <v-card class="mx-auto" max-width="400">
+      <v-card-title>Connexion</v-card-title>
+      <v-card-text>
+        <v-form>
+          <v-text-field
+            v-model="email"
+            label="Email"
+            required
+          ></v-text-field>
+          <v-text-field
+            v-model="password"
+            label="Mot de passe"
+            type="password"
+            required
+          ></v-text-field>
+        </v-form>
+      </v-card-text>
+      <v-card-actions>
+        <v-btn color="primary" @click="login">Se connecter</v-btn>
+        <v-btn to="/register">S'inscrire</v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-container>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const email = ref('')
+const password = ref('')
+
+const login = () => {
+  // TODO: Implémenter la logique de connexion
+}
+</script> 
---
+++ frontend/src/views/RegisterView.vue
@@ -0,0 +1,44 @@
+<template>
+  <v-container>
+    <v-card class="mx-auto" max-width="400">
+      <v-card-title>Inscription</v-card-title>
+      <v-card-text>
+        <v-form>
+          <v-text-field
+            v-model="email"
+            label="Email"
+            required
+          ></v-text-field>
+          <v-text-field
+            v-model="password"
+            label="Mot de passe"
+            type="password"
+            required
+          ></v-text-field>
+          <v-text-field
+            v-model="confirmPassword"
+            label="Confirmer le mot de passe"
+            type="password"
+            required
+          ></v-text-field>
+        </v-form>
+      </v-card-text>
+      <v-card-actions>
+        <v-btn color="primary" @click="register">S'inscrire</v-btn>
+        <v-btn to="/login">Se connecter</v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-container>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const email = ref('')
+const password = ref('')
+const confirmPassword = ref('')
+
+const register = () => {
+  // TODO: Implémenter la logique d'inscription
+}
+</script> 
